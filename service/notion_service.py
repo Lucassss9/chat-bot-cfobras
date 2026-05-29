7b995e29cd57c4e52adbe022457f424c117e89db
lucassss9 <lucasgabriel09104@gmail.com>
2026-05-29 13:53:12 -0300

    fix: utilização de mais de um id, leitura de textos

--- service/notion_service.py
+++ service/notion_service.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 from notion_client import Client
 from notion_client.helpers import iterate_paginated_api
 
-def buscar_pagina():
+def buscar_pagina(id_alvo=None):
     
     load_dotenv()
-
-    lista_de_subpaginas = []
 
     notion_token = os.getenv("NOTION_TOKEN")
     page_id = os.getenv("NOTION_PAGE_ID")
@@ -15,7 +13,15 @@ def buscar_pagina():
     notion = Client(auth=notion_token)
 
     try:
-        response = notion.blocks.children.list(block_id=page_id)
+        lista_de_subpaginas = []
+        tipos_de_texto = ["paragraph", "heading_1", "heading_2", "heading_3", "bulleted_list_item", "numbered_list_item", "callout"]
+        texto_da_pagina = ""
+
+        if id_alvo == None:
+            response = notion.blocks.children.list(block_id=page_id)
+        else:
+            response = notion.blocks.children.list(block_id=id_alvo)
+        
         paginas = response.get("results", [])
 
         for bloco in paginas:
@@ -23,29 +29,23 @@ def buscar_pagina():
             tipo_do_bloco = bloco.get("type")
 
             if tipo_do_bloco == "child_page":
-                id_da_pagina = id_do_bloco
-                lista_de_subpaginas.append(id_da_pagina)
+                titulo_da_pagina = bloco.get("child_page", {}).get("title")
+                lista_de_subpaginas.append({"id": id_do_bloco, "titulo": titulo_da_pagina})
+                buscar_pagina(id_alvo=id_do_bloco)
 
-        for id_subpagina in lista_de_subpaginas:
-            response_subpagina = notion.blocks.children.list(block_id=id_subpagina)
-            subpaginas = response_subpagina.get("results", [])
-            print("==================================")
-            print(f"{len(subpaginas)} subpáginas encontradas para a página com ID: {id_subpagina}")
 
-            for subpagina in subpaginas:
-                id_da_subpagina = subpagina.get("id")
-                tipo_da_subpagina = subpagina.get("type")
-                print(f"ID da subpágina: {id_da_subpagina}")
-                print(f"Tipo da subpágina: {tipo_da_subpagina}")
+            if tipo_do_bloco in tipos_de_texto:
+                texto = bloco.get(tipo_do_bloco, {}).get("rich_text", [])
+                
+                if texto == []:
+                    continue
             
-            for bloco_conteudo in subpaginas:
-                tipo_conteudo = bloco_conteudo.get("type")
-
-                if tipo_conteudo == "paragraph":
-                    texto_conteudo = bloco_conteudo.get("paragraph", {}).get("rich_text", [])
+                texto_da_pagina += texto[0].get("plain_text") + "\n"
 
 
         print(lista_de_subpaginas)
+        print(f"--- CONTEÚDO DA PÁGINA ({id_alvo}): ---")
+        print(texto_da_pagina)
     except Exception as erro:
         print(f"O erro real que aconteceu foi: {erro}")
 
