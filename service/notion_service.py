3fec9906aa18937f097a2f355e0874d264f53d92
lucassss9 <lucasgabriel09104@gmail.com>
2026-05-29 10:49:58 -0300

    feat: implementar conexao com Notion e mapeamento de subpaginas

--- service/notion_service.py
+++ service/notion_service.py
@@ -4,7 +4,10 @@ from notion_client import Client
 from notion_client.helpers import iterate_paginated_api
 
 def buscar_pagina():
+    
     load_dotenv()
+
+    lista_de_subpaginas = []
 
     notion_token = os.getenv("NOTION_TOKEN")
     page_id = os.getenv("NOTION_PAGE_ID")
@@ -15,9 +18,35 @@ def buscar_pagina():
         response = notion.blocks.children.list(block_id=page_id)
         paginas = response.get("results", [])
 
-        print(f"Total de blocos encontrados: {len(paginas)}\n")
+        for bloco in paginas:
+            id_do_bloco = bloco.get("id")
+            tipo_do_bloco = bloco.get("type")
 
-    except:
-        print("Paginas não encontradas")
+            if tipo_do_bloco == "child_page":
+                id_da_pagina = id_do_bloco
+                lista_de_subpaginas.append(id_da_pagina)
+
+        for id_subpagina in lista_de_subpaginas:
+            response_subpagina = notion.blocks.children.list(block_id=id_subpagina)
+            subpaginas = response_subpagina.get("results", [])
+            print("==================================")
+            print(f"{len(subpaginas)} subpáginas encontradas para a página com ID: {id_subpagina}")
+
+            for subpagina in subpaginas:
+                id_da_subpagina = subpagina.get("id")
+                tipo_da_subpagina = subpagina.get("type")
+                print(f"ID da subpágina: {id_da_subpagina}")
+                print(f"Tipo da subpágina: {tipo_da_subpagina}")
+            
+            for bloco_conteudo in subpaginas:
+                tipo_conteudo = bloco_conteudo.get("type")
+
+                if tipo_conteudo == "paragraph":
+                    texto_conteudo = bloco_conteudo.get("paragraph", {}).get("rich_text", [])
+
+
+        print(lista_de_subpaginas)
+    except Exception as erro:
+        print(f"O erro real que aconteceu foi: {erro}")
 
 buscar_pagina()
