3de776b4fd99fcbee812ecf8e2a3251df443ab34
lucassss9 <lucasgabriel09104@gmail.com>
2026-05-29 15:11:14 -0300

    refactor: corrige extração de texto do Notion e adiciona recursão

--- service/notion_service.py
+++ service/notion_service.py
@@ -14,7 +14,8 @@ def buscar_pagina(id_alvo=None):
 
     try:
         lista_de_subpaginas = []
-        tipos_de_texto = ["paragraph", "heading_1", "heading_2", "heading_3", "bulleted_list_item", "numbered_list_item", "callout"]
+        tipos_de_texto = ["paragraph", "heading_1", "heading_2", "heading_3", "bulleted_list_item", "numbered_list_item", "callout", "quote"]
+        tipos_fora_da_lista = []
         texto_da_pagina = ""
 
         if id_alvo == None:
@@ -33,19 +34,27 @@ def buscar_pagina(id_alvo=None):
                 lista_de_subpaginas.append({"id": id_do_bloco, "titulo": titulo_da_pagina})
                 buscar_pagina(id_alvo=id_do_bloco)
 
+            if tipo_do_bloco not in tipos_de_texto and tipo_do_bloco != "child_page":
+                tipos_fora_da_lista.append(tipo_do_bloco)
 
             if tipo_do_bloco in tipos_de_texto:
+                linha_completa = ""
+
                 texto = bloco.get(tipo_do_bloco, {}).get("rich_text", [])
-                
+
                 if texto == []:
                     continue
-            
-                texto_da_pagina += texto[0].get("plain_text") + "\n"
+                
+                for pedacos in texto:
+                    linha_completa += pedacos.get("plain_text")
+
+                texto_da_pagina += linha_completa + "\n"
 
 
         print(lista_de_subpaginas)
-        print(f"--- CONTEÚDO DA PÁGINA ({id_alvo}): ---")
+        print(f"Conteúdo da página ({id_alvo}): ")
         print(texto_da_pagina)
+        print(f"Tipos de bloco que não estão na lista: {set(tipos_fora_da_lista)}")
     except Exception as erro:
         print(f"O erro real que aconteceu foi: {erro}")
 
