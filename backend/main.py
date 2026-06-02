b9bd07ac94c03068c1166fc1824ba607494ec121
lucassss9 <lucasgabriel09104@gmail.com>
2026-06-02 16:41:14 -0300

    feat: rota de cadastro de usuários

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.chat_routes import registrar_rotas
+from routes.usuario_routes import router as usuario_router
 
 app = FastAPI()
 
 registrar_rotas(app)
+app.include_router(usuario_router)
