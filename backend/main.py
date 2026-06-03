0a99d2f6d30e2db2fe19065f083bddcb50167e5a
lucassss9 <lucasgabriel09104@gmail.com>
2026-06-03 08:44:02 -0300

    feat: adiciona CORS no main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.chat_routes import registrar_rotas
 from routes.usuario_routes import router as usuario_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 registrar_rotas(app)
 app.include_router(usuario_router)
+
