483308cca992afdab4b1c9f8b98f465fa0ce3ed1
lucassss9 <lucasgabriel09104@gmail.com>
2026-06-03 08:43:46 -0300

    fix: padroniza retornos do usuario_service para dicionários

--- backend/service/usuario_service.py
+++ backend/service/usuario_service.py
@@ -6,56 +6,57 @@ def cadastrar_usuario(nome, email, senha):
     senha = senha.strip()
 
     erro = _validar_vazio(nome, email, senha)
+    print(f"Erro validação: {erro}")
 
     if erro:
         return erro
 
     if not _validar_email(email):
-        return "Email inválido"
+        return {"sucesso": False, "mensagem": "E-mail inválido"}
 
     if not _validar_senha(senha):
-        return "Senha inválida"
+        return {"sucesso": False, "mensagem": "Senha inválida"}
 
     usuario = buscar_usuario_por_email(email)
 
     if usuario is None:
         salvar_usuario(nome, email, senha)
-        return "Usuário cadastrado com sucesso"
+        return {"sucesso": True, "mensagem": "Usuário cadastrado com sucesso"}
     else:
-        return "Usuário já existe"
+        return {"sucesso": False, "mensagem": "Usuário já existe"}
 
 def login(email, senha):
     email = email.strip().lower()
     senha = senha.strip()
 
     if not email:
-        return "Email obrigatório"
+        return {"mensagem": "E-mail Obrigatorio"}
 
     if not senha:
-        return "Senha obrigatória"
+        return {"mensagem": "Senha Obrigatoria"}
 
     usuario = buscar_usuario_por_email(email)
 
     if usuario is None:
-        return "Faça o cadastro"
+        return {"mensagem": "Faça o Cadastro"}
 
     if usuario.senha == senha:
-        return  "Senha correta"
+        return {"id": usuario.id, "nome": usuario.nome}
     else:
-        return "Senha incorreta"
+        return {"mensagem": "Senha incorreta"}
 
 def _validar_email(email):
-    return "@" in email and ".com" in email
+    return "@" in email and ".com" in email or ".net" in email
 
 def _validar_senha(senha):
     return len(senha) >= 8
 
 def _validar_vazio(nome, email, senha):
     if not nome:
-        return "Nome obrigatório"
+        return {"sucesso": False, "mensagem": "Nome Obrigatorio"}
 
     if not email:
-        return "Email obrigatório"
+        return {"sucesso": False, "mensagem": "E-mail Obrigatorio"}
 
     if not senha:
-        return "Senha obrigatória"
+        return {"sucesso": False, "mensagem": "Senha Obrigatoria"}
