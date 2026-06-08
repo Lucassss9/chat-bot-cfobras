e8c37844b986a00953a3bba09ff01dbbf56377d7
lucassss9 <lucasgabriel09104@gmail.com>
2026-06-08 09:08:25 -0300

    refactor: usuario_repository propaga exceções em vez de retornar

--- backend/repository/usuario_repository.py
+++ backend/repository/usuario_repository.py
@@ -1,7 +1,6 @@
 from model.usuario_model import Usuario
 import bcrypt
 
-
 def salvar_usuario(nome, email, senha, db):
     print(f"Salvando: {nome}, {email}")
     try:
@@ -9,22 +8,17 @@ def salvar_usuario(nome, email, senha, db):
         novo_usuario = Usuario(nome=nome, email=email, senha=senha)
         db.add(novo_usuario)
         db.commit()
-    except Exception as e:
+    except:
         db.rollback()
-        return e
+        raise
 
 def buscar_usuario_por_email(email, db):
-    try:
-        usuario = db.query(Usuario).filter(Usuario.email == email).first()
-        return usuario
-    except Exception as e:
-        db.rollback()
-        return e
+     return db.query(Usuario).filter(Usuario.email == email).first()
 
 def buscar_usuario_por_id(usuario_id, db):
     try:
         usuario = db.query(Usuario).filter(Usuario.id == usuario_id).first()
         return usuario
-    except Exception as e:
+    except:
         db.rollback()
-        return e
+        raise
